70a09373101a3750fe23d121784b8c9e987eec39
ZiadBadr1 <103049045+ZiadBadr1@users.noreply.github.com>
2024-04-12 23:46:38 +0200

    Finish product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+    protected $fillable = ['name', 'description', 'image', 'price', 'quantity', 'category_id', 'sub_category_id'];
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function subCategory():BelongsTo
+    {
+        return $this->belongsTo(SubCategory::class,'sub_category_id');
+    }
+
 }
